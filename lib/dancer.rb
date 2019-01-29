dfb11019f7ef27c198d43ab8ac908ccc59a9f2a1
Mitchel Parish <mitchelparish@gmail.com>
2019-01-29 19:51:54 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,10 +3,4 @@ require_relative './fancy_dance.rb'
 class Dancer
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
-
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
 end
